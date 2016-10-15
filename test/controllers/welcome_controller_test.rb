3a30641e72485e39e3cc0470ec6562320b7fd552
Dan Charlesworth <dcharlesworth@me.com>
2016-10-15 17:21:04 +1100

    Add dynamic <title>s to the Welcome views
    Perhaps there’s a DRYer way?

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Day Book"
+  end
+
   test "can get the welcome page" do
     get "/"
     assert_response :success
+    assert_select "title", "Welcome to #{@base_title}"
   end
 
   test "can get the about page" do
     get welcome_about_url
     assert_response :success
+    assert_select "title", "About #{@base_title}"
   end
 end
