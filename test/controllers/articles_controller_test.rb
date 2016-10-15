dbc48637210bb4cb42785ae503136747fd0c078c
Dan Charlesworth <dcharlesworth@me.com>
2016-10-15 18:07:29 +1100

    Add controller tests for Articles
    These are *mostly* scaffolded, but I undestand what it’s all doing.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,58 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "can get articles" do
+  setup do
+    @article = articles(:one)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
+  test "should get articles index" do
     get articles_url
     assert_response :success
+  end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference("Article.count") do
+      post articles_url, params: { article: { title: "Thank You!",
+                                               text: "Today I feel inspired."} }
+    end
+
+    assert_redirected_to article_url(Article.last)
+  end
+
+  test "should show article" do
+    get article_url(@article)
+    assert_response :success
+  end
+
+  test "should get article editor" do
+    get edit_article_url(@article)
+    assert_response :success
+  end
+
+  test "should update article" do
+    patch article_url(@article), params: { article: { title: "Flows",
+                                                       text: "The dimension of time"} }
+    assert_redirected_to article_url(@article)
+    # Reload association to fetch updated data and assert that title is updated
+    @article.reload
+    assert_equal "Flows", @article.title
+  end
+
+  test "should destroy article" do
+    assert_difference("Article.count", -1) do
+      delete article_url(@article)
+    end
+
+    assert_redirected_to articles_url
+    assert_equal "✔︎ The article was deleted", flash[:notice]
   end
 end
