b8658acba4616c1caa6b1dfe73e925fd9148421d
Dan Charlesworth <dcharlesworth@me.com>
2016-11-14 16:12:22 +1100

    Add signup controller and page stub

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
-  get 'welcome/index'
-  get '/about', to: 'welcome#about'
+  get  'welcome/index'
+  get  '/about', to: 'welcome#about'
+
+  get  '/signup', to: 'users#new'
+  
 
   resources :articles do
     resources :comments
