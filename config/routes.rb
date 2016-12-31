4cecb9c1831a832d5daa4c4b885a002efb89b37c
Dan Charlesworth <dcharlesworth@me.com>
2016-12-31 16:36:06 +1100

    Wire up the sign-up form with controllers + views
    In other words, it works now. All finished.
    
    👨‍🎨
    
    - Includes tests, obv.
    - Adds a helper for fetching users’ gravatars
    - Adds a (commented) debug readout to the application template layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  # Welcome
   root 'welcome#index'
   get  'welcome/index'
   get  '/about', to: 'welcome#about'
 
+  # Users
   get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 
+  # Articles
   resources :articles do
     resources :comments
   end
