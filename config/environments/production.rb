2fa1abc985fa1e4c1c094a2bf7aedf8cf6abbd2b
Dan Charlesworth <dcharlesworth@me.com>
2017-05-25 18:46:47 +1000

    ㊙️ Enable encrypted secrets for production
    Thanks rails 5.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Keep them secret, keep them safe (Dan has the key)
+  config.read_encrypted_secrets = true
 end
