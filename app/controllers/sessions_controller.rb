7b496a758bdba484c0dc8b5c09cd7aafdd6fbff1
Dan Charlesworth <dcharlesworth@me.com>
2019-10-02 13:28:21 +1000

    Update sessions controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,18 +7,18 @@ class SessionsController < ApplicationController
   # POST /sign_in
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
+
+    if correct_password
       log_in @user
       remember(@user)
-      # Redirect to the user's original destination
       redirect_back_or @user
-    elsif
-      @user && !@user.authenticate(params[:session][:password])
-      flash.now[:error] = "❌ We didn't recognise that password. \
-                           If you've forgotten, you can #{view_context.link_to("reset it here", forgot_password_path)}".html_safe
+    elsif incorrect_password
+      flash.now[:error] = "We don't recognise that password. \
+                           If you've forgotten, you can \
+                           #{view_context.link_to("reset it here", forgot_password_path)}".html_safe
       render 'new'
     else
-      flash.now[:error] = "❌ We didn't recognise that email, sorry"
+      flash.now[:error] = "We don't recognise that email, sorry."
       render 'new'
     end
   end
@@ -29,4 +29,13 @@ class SessionsController < ApplicationController
     flash[:notice] = '👋 See you again soon!'
     redirect_to root_url
   end
+
+  private
+    def correct_password
+      @user && @user.authenticate(params[:session][:password])
+    end
+
+    def incorrect_password
+      @user && !@user.authenticate(params[:session][:password])
+    end
 end
